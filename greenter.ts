343dd3c50933102bd9020fcd2c4d7322d90516be
yu <18113406084@163.com>
2019-09-12 18:09:12 +0800

    typeScript字符类型学习

---
+++ greenter.ts
@@ -0,0 +1,22 @@
+class User {
+    fullName: string
+    firstName: string
+    lastName: string
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.fullName = firstName + ' ' + lastName
+    }
+}
+
+interface Person {
+    firstName: string,
+    lastName: string
+}
+
+function greeter(person: Person) {
+    return 'hello ' + person.firstName + ' ' + person.lastName
+}
+let user = new User('yu', 'yunchao')
+console.log(greeter(user));
