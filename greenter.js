1489d0b0febae91a448a25197398c8fe8c07a02a
yu <18113406084@163.com>
2019-10-08 11:17:40 +0800

    类与接口

--- greenter.js
+++ greenter.js
@@ -1,11 +1,10 @@
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
-    }
-    Person.prototype.sayHi = function () {
-        return "my name is " + this.name;
-    };
-    return Person;
-}());
-var a = new Person('Jack');
-console.log(a.sayHi());
+function getCounter() {
+    var counter = function (start) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+var c = getCounter();
+c(10);
+c.reset();
+c.interval = 5.0;
--- greenter.ts
+++ greenter.ts
@@ -1,11 +1,16 @@
-class Person {
-    name: string
-    constructor(name:string) {
-        this.name = name
-    }
-    sayHi():string {
-        return `my name is ${this.name}`
-    }
+interface Counter {
+    (start: number): string
+    interval: number
+    reset(): void
 }
-let a:Person = new Person('Jack')
-console.log(a.sayHi()); //my name is Jack
+
+function getCounter():Counter {
+    let counter = <Counter>function(start: number) {}
+    counter.interval = 123
+    counter.reset = function(){}
+    return counter
+}
+let c = getCounter()
+c(10)
+c.reset()
+c.interval = 5.0
